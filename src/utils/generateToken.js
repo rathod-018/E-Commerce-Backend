093f34c3f016556ffff64ece71de9c89b71cf8d4
Shreyas K <shrathod6362@gmail.com>
2025-09-23 18:14:07 +0530

    add generate token util function

---
+++ src/utils/generateToken.js
@@ -0,0 +1,23 @@
+import { User } from "../models/user.models"
+import { ApiError } from "./ApiError"
+
+const generateAccessTokenAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken
+
+        await user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
+
+
+    } catch (error) {
+        console.log(error.message)
+        throw new ApiError(400, "Error while generation access and refresh token")
+    }
+}
+
