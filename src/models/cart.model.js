d13357b599cbbcd2fa7bb06421b36d1cfd067a0b
Shreyas K <shrathod6362@gmail.com>
2025-09-29 14:40:10 +0530

    add cart model

---
+++ src/models/cart.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+
+
+const cartItemSchema = new Schema(
+    {
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product"
+        },
+        quantity: {
+            type: Number,
+            default: 1,
+            min: 1
+        }
+    },
+    {
+        _id: false
+    })
+
+
+const cartSchema = new Schema(
+    {
+        userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        items: [cartItemSchema],
+        status: {
+            type: Boolean,
+            enum: ["active", "ordered"],
+            default: "active"
+        }
+    },
+    {
+        timestamps: true
+    })
+
+
+export const Cart = mongoose.model("Cart", cartSchema)
