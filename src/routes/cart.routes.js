04ced07bf481362995ad985ae7a398dfaf0497f5
Shreyas K <shrathod6362@gmail.com>
2025-09-29 17:08:53 +0530

    add ckeckout route and fix request methods

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -3,6 +3,7 @@ import { verifyJWT } from "../middleware/auth.middleware.js";
 import { verifyRole } from "../middleware/verifyRole.middleware.js"
 import {
     addCartItem,
+    checkout,
     getCart,
     removeCartItem,
     updateCartItem
@@ -11,17 +12,19 @@ import {
 const router = Router()
 
 router.use(verifyJWT)
-
 router.use(verifyRole("customer"))
+
+
 
 router.route("/add-item/:productId").post(addCartItem)
 
-router.route("/update-item/:productId").post(updateCartItem)
+router.route("/update-item/:productId").patch(updateCartItem)
 
-router.route("/remove-item/:productId").post(removeCartItem)
+router.route("/remove-item/:productId").patch(removeCartItem)
 
-router.route("/").post(getCart)
+router.route("/").get(getCart)
 
 // checkout route
+router.route("/checkout").post(checkout)
 
 export default router
