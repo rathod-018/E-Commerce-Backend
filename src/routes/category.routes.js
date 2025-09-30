50130779dcae84762417a0cbdb1d71d7fce8d54f
Shreyas K <shrathod6362@gmail.com>
2025-09-30 22:53:43 +0530

    fix request method

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { verifyRole } from "../middleware/verifyRole.middleware.js"
-import { createCategory, deleteCategory, getAllCategories, updateCategory } from "../controllers/categories.controller";
+import { createCategory, deleteCategory, getAllCategories, updateCategory } from "../controllers/categories.controller.js";
 
 const router = Router()
 
@@ -12,13 +12,13 @@ router.route("/").get(getAllCategories)
 
 // admin only routes
 
-router.get(verifyRole("admin"))
+router.use(verifyRole("admin"))
 
 router.route("/create").post(createCategory)
 
 router.route("/update/:categoryId").patch(updateCategory)
 
-router.route("/delete/:categoryId").patch(deleteCategory)
+router.route("/delete/:categoryId").delete(deleteCategory)
 
 
 export default router
